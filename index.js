2e1b1870eaad56bd48cdb1dd8a72be40cef093ca
Martin <martintonts@gmail.com>
2020-03-31 13:08:17 +0300

    added more logging, and simple endpoint for uptime check ("/status")

--- index.js
+++ index.js
@@ -9,24 +9,38 @@ const HOST = '0.0.0.0';
 
 var url = "https://xgis.maaamet.ee/adsavalik/valjav6te/";
 
+function getNow() {
+  return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+}
 // App
 const app = express();
 app.get('/', (req, res) => {
-  request({
-    url: url,
-    json: true
-  }, function (error, response, body) {
-    var item = body.filter(function(i){return i.vvnr == 1 && !("kov" in i);});
-    url = url + item[0].fail;
-    console.log(url);
-    res.set({
-      'Content-Type': 'application/zip'
-    });
-    request(url).pipe(res);
-  })
+  var item;
+  console.log(getNow() + ' - Sending request to ' + url);
+  request({url: url, json: true}, function (error, response, body) {
+    if (error) {
+      return console.error(getNow() + ' - Failed, error: ' + error);
+    }
+    if (response) console.log(getNow() + ' - Got response from ' + url + ', response: ' + response.statusCode);
+    if (body) {
+      item = body.filter(function(i){return i.vvnr == 1 && !("kov" in i);});
+      url = url + item[0].fail;
+      res.set({
+        'Content-Type': 'application/zip'
+      });
+      console.log(getNow() + ' - Sending request to ' + url);
+      request(url, function (err, resp, body2)  {
+        if (err) return console.error(getNow() + ' - Failed, error: ' + err);
+        if (resp) console.log(getNow() + ' - Got response from ' + url + ', response: ' + resp.statusCode);
+      }).pipe(res);
+    }
+  });
+});
+
+app.get('/status', (req, res) => {
+  res.statusCode = 200;
+  res.end('Server running, current time: ' + getNow());
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-
