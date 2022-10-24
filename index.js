741f9a0130d0669a78738867be9ed5f487313fca
Martin <martintonts@gmail.com>
2022-10-24 09:49:48 +0300

    REIS-142 - ads huvipunktide otsingusse importimine

--- index.js
+++ index.js
@@ -36,6 +36,29 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/huvipunktid', (req, res) => {
+  var url = "https://xgis.maaamet.ee/adsavalik/valjav6te/";
+  console.log(getNow() + ' - Sending request to ' + url);
+  request(url, function (error, response, body) {
+    if (error) {
+      return console.error(getNow() + ' - Failed, error: ' + error);
+    }
+    if (response) console.log(getNow() + ' - Got response from ' + url + ', response: ' + response.statusCode);
+    if (body) {
+      let item = JSON.parse(body).filter(function(i){return i.vvnr == 18 && !("kov" in i);});
+      url = url + item[0].fail;
+      res.set({
+        'Content-Type': 'application/zip'
+      });
+      console.log(getNow() + ' - Sending request to ' + url);
+      request(url, function (err, resp, body2)  {
+        if (err) return console.error(getNow() + ' - Failed, error: ' + err);
+        if (resp) console.log(getNow() + ' - Got response from ' + url + ', response: ' + resp.statusCode);
+      }).pipe(res);
+    }
+  });
+});
+
 app.get('/status', (req, res) => {
   res.statusCode = 200;
   res.end('Server running, current time: ' + getNow());
